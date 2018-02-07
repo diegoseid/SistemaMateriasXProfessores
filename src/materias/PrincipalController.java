0e0f3c72b72e2e967de6c726b4bdf0e9f7b4a210
Diego Seid <Diego Seid@Diego>
2018-02-07 16:21:16 -0200

    Incio, Botões Pré Configurados com funcoes de deixar visivel ou nao

--- src/materias/PrincipalController.java
+++ src/materias/PrincipalController.java
@@ -7,7 +7,12 @@ package materias;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -16,12 +21,27 @@ import javafx.fxml.Initializable;
  */
 public class PrincipalController implements Initializable {
 
+    @FXML private Pane P_Marcar;
+    @FXML private Button but_MarcarProva;
+    @FXML private Button but_Agendar;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+      but_Agendar.setOnMouseClicked(((event) -> {
+          P_Marcar.setVisible(true);
+          but_MarcarProva.setVisible(true);
+      }));
+      
+      but_MarcarProva.setOnMouseClicked(((event) -> {
+         P_Marcar.setVisible(false);
+          but_MarcarProva.setVisible(false); 
+      }));
+        
     }    
     
 }
